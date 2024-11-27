9f8d2c523f59c33fd05224adeec364f241d6d83a
Jorge Rivera <jorivera@gmail.com>
2024-11-26 19:42:58 -0500

    Otro cambio

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home 1</RouterLink>
+        <RouterLink to="/">Home nuevo</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>

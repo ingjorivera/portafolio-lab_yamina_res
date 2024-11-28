3c0c01a4c4d525106df578c2a25eb713d933d961
Jorge Rivera <jorivera@gmail.com>
2024-11-28 16:54:31 -0500

    Avance

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,31 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="column"
+    style="width: 100vw; height: 100vh; background: linear-gradient(rgba(255,255,255,0.55), rgba(255,255,255,0.55)), url('./images/bg_web-min.png');">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="col items-center justify-center row">
+      <div class="bg-white row shadow-6" style="border-radius: 20px; overflow: hidden;">
+        <div class="q-pa-md" style="width: 370px; background: url('./images/bg_left-min.png'); min-height: 470px;">
+        </div>
+        <div class="q-pa-lg bg-white column items-center " style="width: 620px;">
+          <div class="text-h5 text-bold" style="text-align: center;">Resultados precisos y confiables <br> a un clic de
+            distancia
+          </div>
+          <div class="q-pt-lg" style="max-width:75% ; text-align: center;">
+            Bienvenido al portal de resultados del Laboratorio Especializado Yamina Cumplido, aquí podras descargar tus
+            resultados de forma sencilla, rapida y segura.
+          </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="row justify-center" style="height: 50px;">
+      Todos los derechos reservados
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/es'
+import quasarIconSet from 'quasar/icon-set/fontawesome-v5'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +22,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    lang: quasarLang,
+    iconSet: quasarIconSet,
+  })
 
 app.mount('#app')

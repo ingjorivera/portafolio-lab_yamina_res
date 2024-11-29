651cd354f91d820f9d63a94abb353794ac5cc065
Jorge Rivera <jorivera@gmail.com>
2024-11-28 20:09:44 -0500

    adelanto ui

---
+++ src/components/Results.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="column " style="width: 100%; height: 100%; gap:10px">
+        <div class="text-h5 text-bold q-pb-md">Hola Jorge,</div>
+        <div>Estos son tus resultados ordenados por fecha de creación.</div>
+        <div class="column col bg-blue bg-blue-grey-1" style="border-radius: 8px; overflow: hidden;">
+            <div class="bg-blue q-pa-sm text-white" style="font-size: x-small; font-weight: bold;">LISTA DE RESULTADOS
+
+            </div>
+            <div class="col">
+
+            </div>
+        </div>
+    </div>
+</template>
